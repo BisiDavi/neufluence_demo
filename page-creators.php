c4756eb740ad2d2e5bd178b46be52a9eeafa0765
BisiDavi <oludavidconnect@gmail.com>
2023-05-28 01:39:56 +0100

    creators page added

---
+++ page-creators.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ * @package Wordpress
+ * @subpackage Neufluence Theme
+ */
+
+get_header();
+
+get_template_part('template-parts/home/hero-banner', 'hero-banner', array(
+    "page_id" => 9,
+    "class" => 'creators'
+));
+?>
