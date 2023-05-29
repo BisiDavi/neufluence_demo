0a677793b56b616dc7126bf719ab4689f1663b0b
BisiDavi <oludavidconnect@gmail.com>
2023-05-29 04:42:50 +0100

    footer updated

--- footer.php
+++ footer.php
@@ -3,31 +3,58 @@
     <div class="footer_bg"></div>
     <? get_template_part('template-parts/newsletter', 'newsletter'); ?>
     
-    <hr class="footer_divider" />
-
     <div class="footer_content">
         <div class="footer_logo">
             <?php the_custom_logo(); ?>
             <p>© 2021 Neufluence | All Rights reserved</p>
         </div>
-        <div class="company">
-            <h5>Company</h5>
-            <ul>
-                <li>Abous Us</li>
-                <li>Privacy Policy</li>
-                <li>Services</li>
-                <li>Terms of  Use</li>
-            </ul>
+        
+        <div class="footer_links">
+            <div class="company">
+                <h5>Company</h5>
+                <ul>
+                    <li>About Us</li>
+                    <li>Privacy Policy</li>
+                    <li>Services</li>
+                    <li>Terms of  Use</li>
+                </ul>
+            </div>
+            <div class="company">
+                <h5>Resources</h5>
+                <ul>
+                    <li>Knowledge Base</li>
+                    <li>Blog</li>
+                    <li>Press</li>
+                    <li>FAQs</li>
+                </ul>
+            </div>
         </div>
-        <div class="company">
-            <h5>Resources</h5>
-            <ul>
-                <li>Knowledge Base</li>
-                <li>Blog</li>
-                <li>Press</li>
-                <li>FAQs</li>
-            </ul>
+        
+        <div class="socials">
+            <!-- instagram -->
+            <span>
+        <svg class="tcb-icon" viewBox="0 0 448 512" data-id="instagram" data-name="">
+            <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
+        </svg>
+        </span>
+        <!-- facebook -->
+        <span>
+        <svg class="tcb-icon" viewBox="0 0 264 512" data-id="facebook" data-name="">
+            <path d="M76.7 512V283H0v-91h76.7v-71.7C76.7 42.4 124.3 0 193.8 0c33.3 0 61.9 2.5 70.2 3.6V85h-48.2c-37.8 0-45.1 18-45.1 44.3V192H256l-11.7 91h-73.6v229"></path>
+        </svg>
+        </span>
+        <!-- linkedin -->
+        <span>
+        <svg class="tcb-icon" viewBox="0 0 448 512" data-id="linkedin" data-name="">
+            <path d="M100.3 480H7.4V180.9h92.9V480zM53.8 140.1C24.1 140.1 0 115.5 0 85.8 0 56.1 24.1 32 53.8 32c29.7 0 53.8 24.1 53.8 53.8 0 29.7-24.1 54.3-53.8 54.3zM448 480h-92.7V334.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V480h-92.8V180.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V480z"></path>
+        </svg>
+        </span>
+        <!-- tiktok -->
+        <span>
+        <svg class="tcb-icon" viewBox="0 0 448 512" data-id="icon-tiktok-brands" data-name="">
+            <path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"></path></svg>
         </div>
+        </span>
     </div>
     </footer>
 </body>
